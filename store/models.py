6d0dfbfe689a388914b536c423dd6636d4fe1c55
Esayas188 <esayasfekade188@gmail.com>
2023-12-19 01:31:10 -0500

    feat(model): add two property on order model

--- store/models.py
+++ store/models.py
@@ -42,3 +42,15 @@ class Order(models.Model):
 				shipping = True
 		return shipping
 
+	@property
+	def get_cart_total(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.get_total for item in orderitems])
+		return total 
+
+	@property
+	def get_cart_items(self):
+		orderitems = self.orderitem_set.all()
+		total = sum([item.quantity for item in orderitems])
+		return total 
+
