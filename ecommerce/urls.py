005eb4fbd8cc91db498be0b5b228c2725bcb5482
Esayas188 <esayasfekade188@gmail.com>
2023-12-19 06:28:12 -0500

    style(line):remove line

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -24,7 +24,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('store.urls')),
-
     path("__reload__/", include("django_browser_reload.urls")),
 
 ]
