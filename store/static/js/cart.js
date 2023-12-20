ad7b77c74cbc2f2c4202caabf3e155272dad6b9b
Esayas188 <esayasfekade188@gmail.com>
2023-12-20 03:54:54 -0500

    feat(javascript): add eventlistener

--- store/static/js/cart.js
+++ store/static/js/cart.js
@@ -1,1 +1,17 @@
-console.log('hellow this is from js');
+console.log('this is from my data')
+document.addEventListener("DOMContentLoaded", function() {
+ 
+    
+    console.log("DOM fully loaded and parsed");
+
+    var updateBtns = document.getElementsByClassName('update-cart');
+
+    Array.from(updateBtns).map(function(btn) {
+        btn.addEventListener('click', function() {
+            var productId = this.dataset.product;
+            var action = this.dataset.action;
+            console.log('productId:', productId, 'Action:', action);
+
+        });
+    });
+});
